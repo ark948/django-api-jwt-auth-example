86a884e0f6d9ac33e02bc30c9e21e0750b701486
ark948 <armankarimi948@gmail.com>
2024-08-14 20:06:46 +0330

    Added User serializer

---
+++ backend/api/serializers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        # do not return password field as response
+        # get password from user, but do not return it
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
